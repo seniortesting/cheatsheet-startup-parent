de0047d58e245427efa992f0f19c30bdec4f70f4
Walter Hu <alterhu2020@gmail.com>
2021-10-24 12:20:31 +0800

    feat: provider annotation

--- cheatsheet-startup-java/google-guice/src/main/java/com/example/guice/providers/annotation/ProvideAnnotationModule.java
+++ cheatsheet-startup-java/google-guice/src/main/java/com/example/guice/providers/annotation/ProvideAnnotationModule.java
@@ -5,6 +5,7 @@ import com.example.guice.providers.IProviderServiceImpl;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.google.inject.name.Named;
 
 public class ProvideAnnotationModule extends AbstractModule {
 
@@ -12,6 +13,14 @@ public class ProvideAnnotationModule extends AbstractModule {
     @Provides
     @Singleton
     public IProviderService getService(){
+        return new IProviderServiceImpl();
+    }
+
+
+    @Named("alipay")
+    @Provides
+    @Singleton
+    public IProviderService getService2(){
         return new IProviderServiceImpl();
     }
 }
--- cheatsheet-startup-java/google-guice/src/test/java/com/example/guice/providers/annotation/ProvideAnnotationDemoTest.java
+++ cheatsheet-startup-java/google-guice/src/test/java/com/example/guice/providers/annotation/ProvideAnnotationDemoTest.java
@@ -4,6 +4,7 @@ import com.example.guice.providers.IProviderService;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
+import com.google.inject.name.Names;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,5 +17,8 @@ class ProvideAnnotationDemoTest {
         Injector injector = Guice.createInjector(new ProvideAnnotationModule());
         IProviderService providerService = injector.getInstance(Key.get(IProviderService.class, PayPal.class));
         providerService.runData("sample data");
+
+        IProviderService alipay = injector.getInstance(Key.get(IProviderService.class, Names.named("alipay")));
+        alipay.runData("alipay");
     }
 }
